77737f064494238cc24556672b76f262678062cc
karan singh D <ceitkaransingh2026@gmail.com>
2024-03-19 06:35:56 +0530

    api conept in react

---
+++ apiintegration/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  const [advice, setadvice] = useState("please click the button to get the advice")
+
+async function advices(){
+  const res = await fetch(`https://api.adviceslip.com/advice`)
+  const data=await res.json()
+  setadvice(data.slip.advice)
+  
+
+  console.log(res)
+
+}
+
+useEffect(()=>{
+  advices()
+},[])
+
+  return (
+    <>
+        <h1>advice-shop</h1>
+        <h3>{advice}</h3>
+        <button onClick={advices}>Get advice : </button>  
+    </>
+  )
+}
+
+export default App
