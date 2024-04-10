d85d05e0fbc1b3134b1f1a8bf89cb5ee72cb7cc9
karan singh D <ceitkaransingh2026@gmail.com>
2024-04-10 23:40:51 +0530

    todo app

--- calendar/src/App.jsx
+++ calendar/src/App.jsx
@@ -20,27 +20,62 @@ function App() {
     console.log(firstday);
     console.log(lastday);
 
-
+    for (let i=0;i<firstday.getDay();i++){
+      daysarray.push(0);
+    }
+    for(let i=1;i<=lastday.getDate();i++){
+      daysarray.push(new Date(selecteddate.getFullYear(),selecteddate.getMonth(),i));
+    }
     return daysarray;
+  }
+
+  const handlechangemonth=(e)=>{
+    const newmonth=parseInt(e.target.value,10);
+    setselecteddate(new Date(selecteddate.getFullYear(),newmonth,selecteddate.getDate()));
+  }
+  const handleyear=(e)=>{ 
+    const newyear=parseInt(e.target.value,10);
+    setselecteddate(new Date(newyear,selecteddate.getMonth(),selecteddate.getDate()));
+  }
+
+  const isthesameday=(day1,day2)=>{
+    return day1.getFullYear()===day2.getFullYear() && day1.getMonth()===day2.getMonth() && day1.getDate()===day2.getDate();
   }
 
   return (
     <>
      <div className='calendar'>
       <div className="header">
-        <button><img src={ArrowRight} alt="" />right</button>
-        <select value={selecteddate.getMonth()}>
-          {month.map((month,index)=><option key={index} value={index}>{month}</option>)}
-        </select>
-        <select value={selecteddate.getFullYear}>
-          {Array(20).fill(selecteddate.fullY).map((_,index)=><option key={index} value={selecteddate.getFullYear()-5+index}>{selecteddate.getFullYear()-5+index}</option>)}
+        <button onClick={()=>{setselecteddate(new Date(selecteddate.getFullYear(),selecteddate.getMonth()-1,1))}}><img src={ArrowRight} alt="" />right</button>
 
+        <select value={selecteddate.getMonth()} onChange={handlechangemonth}>
+          {month.map((month,index)=>
+          <option key={index} value={index}>{month}</option>
+          )}
         </select>
-        <button><img src={<ArrowLeft color="royalblue"/>} alt="" />left</button>
+
+        <select value={selecteddate.getFullYear} onChange={handleyear}>
+          {
+           Array.from({length:10},(_,i)=>selecteddate.getFullYear()-5+i).map((year,index)=>(
+              <option key={index} value={year}>{year}</option>
+            ))
+          }
+                                                          
+        </select>
+        <button  onClick={()=>{setselecteddate(new Date(selecteddate.getFullYear(),selecteddate.getMonth()+1,1))}} ><img src={<ArrowLeft color="royalblue"/>} alt="" />left</button>
       </div>
 
       <div className="daysofweek">
         {dayofweek.map((day,index)=><div key={index}>{day}</div>)}
+      </div>
+
+      <div className="days">
+        {
+          daysinmonth().map((day,index)=>
+          <div key={index} className={day===0?"empty":"day"}>
+            {day===0?"":day.getDate()}
+          </div>)
+        }
       </div>
      </div>
     </>
